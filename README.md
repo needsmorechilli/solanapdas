f61c017893c96fe3c68dfb4f6a9a06a854b5d5a4
Nat <natalie.catherine.yeo@gmail.com>
2023-06-10 21:00:11 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # solanapdas
+Testing for PDAs on solana
+1. Create bank
+2. Deposit to bank
+3. Withdraw (only admin), basic checks
--- programs/solanapdas/src/lib.rs
+++ programs/solanapdas/src/lib.rs
@@ -3,10 +3,7 @@ use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
-//Purpose
-//1. Create bank
-//2. Deposit to bank
-//3. Withdraw (only admin)
+
 #[program]
 pub mod solanapdas {
     use super::*;
